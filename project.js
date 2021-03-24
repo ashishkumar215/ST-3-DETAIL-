2ae7d554551911d1c3941e5b77752fa28860b4df
ashishkumar215 <77110167+ashishkumar215@users.noreply.github.com>
2021-03-24 16:02:29 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,70 +1,50 @@
-function fun()
-{
 
-    var name=document.getElementById("name");
-    var email=document.getElementById("email");
-    var contact=document.getElementById("mobile");
-
-    var atpos = email.value.indexOf("@");
-    var dotpos = email.value.lastIndexOf(".");
-
-    if(name.value=="" || email.value=="" || contact.value=="")
-    {
-        window.alert("Enter the  required field");
-    }
-    else if(name.value.length<3)
-    {
-        window.alert("Your name must be 3 characters long");
-    }
-    else if(contact.value.length!=10)
-    {
-        window.alert("Enter valid mobile number");
-    }
-    else if(atpos < 1 || ( dotpos - atpos < 2 ))
-    {
-        window.alert("Enter valid email-id");
-    }
-    else
-    {
-        var disp=document.getElementById("last");
-        disp.style.display="block";
-        var table = document.getElementById("detailTable");
-        var count=0;
-    for (let row of table.rows) 
-    {
-        for(let cell of row.cells) 
-        {
-        if(cell.innerText == email.value || cell.innerText==contact.value)
-        count=1;
-        }
-    }
-    if(count==1)
-    alert("User already exists");
-    else
-    {
-    var row = table.insertRow(table.length);
-    row.setAttribute("class","table-success");
-    row.setAttribute("id","delRow")    
-
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-
-    cell1.innerHTML = name.value;
-    cell2.innerHTML = email.value;
-    cell3.innerHTML=contact.value;
-    cell4.innerHTML="<a href='#' onclick='del(delRow)'>Delete</a>";
-
-    name.value='';
-    email.value='';
-    contact.value='';
-    }
+function fun(){
+    var name1=document.getElementById('name').value;
+var mobile=document.getElementById('mobileNumber').value;
+var email=document.getElementById('email').value;
+var table=document.getElementById('table');
+var flag=0;
+console.log(mobile);
+if(name1 == ""){
+    alert("Name cannot be left empty");
+    flag=1;
 }
+if(mobile == "" && flag==0){
+    alert("Mobile Number cannot be left empty");
+    flag=1;
 }
-function del(delRow)
-{
-    var table = document.getElementById("detailTable");
-    delRow.parentNode.removeChild(delRow);
+if(email ==""  && flag==0){
+    alert("Email cannot be left empty");
+    flag=1;
+}
+if(flag==0){
+    
+   var row= document.createElement('tr');
+   var data1= document.createElement('td');
+   var data2= document.createElement('td');
+   var data3= document.createElement('td');
+   var data4= document.createElement('td');
+   var div=document.createElement('div');
+   
+   div.setAttribute("id","blockDiv");
+   div.innerHTML=email;
+   data1.innerHTML= name1;
+   data2.innerHTML= mobile;
+   data3.appendChild(div);
+   data4.innerHTML="<button onclick=del() style='height:28px;width:65px; background-color: antiquewhite;'>Delete</button>";
 
+   row.appendChild(data1);
+   row.appendChild(data2);
+   row.appendChild(data3);
+   row.appendChild(data4);
+   table.appendChild(row);
+}
+
+}
+function del(){
+        var td = event.target.parentNode; 
+        var tr = td.parentNode; 
+        tr.parentNode.removeChild(tr);
+  
 }
